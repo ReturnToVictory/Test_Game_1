ea77f4e0d534e846950602aa5773980c469be444
ReturnToVictory <xamperus@list.ru>
2017-05-11 21:52:40 +0400

    Добавил кнопку.

--- GameTest1/Form1.cs
+++ GameTest1/Form1.cs
@@ -16,5 +16,10 @@ namespace GameTest1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Marat Krasava!");
+        }
     }
 }
